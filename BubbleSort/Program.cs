9d588a8eaf38520f2a3201fcb3d8c90fed3c4727
dvbtham <dvbtham@gmail.com>
2016-06-14 19:53:41 +0700

    xong lan 1

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -10,6 +10,38 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
+            int[] arr = { 3, 1, 5, 4, 7, 9, 2, 8 };
+            Console.Write("\nMang chua sap xep: ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.Write("\nMang da sap xep: ");
+            IntArrayBubbleSort(arr);
+            Console.ReadKey();
+        }
+        public static void exchange(int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data[m];
+            data[m] = data[n];
+            data[n] = temporary;
+        }
+        public static void IntArrayBubbleSort(int[] data)
+        {
+            int i, j;
+            int N = data.Length;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (data[i] > data[i + 1])
+                        exchange(data, i, i + 1);
+                }
+            }
+            for (int n = 0; n < data.Length; n++)
+                Console.Write(data[n] + " ");
         }
     }
 }
