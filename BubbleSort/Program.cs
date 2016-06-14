b7a0ecac70171172eada4803b6a259d3f289980c
dvbtham <dvbtham@gmail.com>
2016-06-14 19:58:17 +0700

    sửa lần 2

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -10,16 +10,18 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            int[] arr = { 3, 1, 5, 4, 7, 9, 2, 8 };
+            int[] arr = { 3, 1, 5, 4, 7, 9, 2, 8 };//Khởi tạo mảng
+            //Mảng chưa sắp xếp
             Console.Write("\nMang chua sap xep: ");
             for (int i = 0; i < arr.Length; i++)
                 Console.Write(arr[i] + " ");
-
+            //Mảng đã sắp xếp
             Console.Write("\nMang da sap xep: ");
             IntArrayBubbleSort(arr);
             Console.ReadKey();
         }
-        public static void exchange(int[] data, int m, int n)
+        //Hàm hoán đổi vị trí
+        public static void Exchange(int[] data, int m, int n)
         {
             int temporary;
 
@@ -37,7 +39,7 @@ namespace BubbleSort
                 for (i = 0; i < j; i++)
                 {
                     if (data[i] > data[i + 1])
-                        exchange(data, i, i + 1);
+                        Exchange(data, i, i + 1);
                 }
             }
             for (int n = 0; n < data.Length; n++)
